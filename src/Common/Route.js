8362f23f1c1f4e97ad812da9ff3507fd6418b62a
jin-Pro <dnjun2@ajou.ac.kr>
2022-05-19 00:06:19 +0900

    refactor: Route Business Logic

--- src/Common/Route.js
+++ src/Common/Route.js
@@ -1,8 +1,26 @@
+import Detail from "../Component/Detail/Detail.js";
+import Main from "../Component/Main/Main.js";
 import { ROUTE_EVENT_NAME } from "./Constant.js";
 
-const Route = (routing) => {
+export const Route = (routing) => {
   window.addEventListener("popstate", routing);
   window.addEventListener(ROUTE_EVENT_NAME, routing);
 };
 
-export default Route;
+export const MAP_CONTAINER = {
+  "/": {
+    html: `<div class="MainPage"></div>`,
+    className: ".MainPage",
+    page: ($target) => new Main({ $target }),
+  },
+  "/detail": {
+    html: `<div class="DetailPage"></div>`,
+    className: ".DetailPage",
+    page: ($target) => new Detail({ $target }),
+  },
+};
+
+export const routeChange = (url) => {
+  history.pushState(null, null, url);
+  window.dispatchEvent(new CustomEvent(ROUTE_EVENT_NAME));
+};
--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
-import Route from "./Common/Route.js";
-import { routeChange } from "./Common/Util.js";
+import { Route, MAP_CONTAINER, routeChange } from "./Common/Route.js";
 
-const init = (() => {
+(() => {
+  const $target = document.querySelector("#root");
+
   const route = () => {
     const { pathname } = location;
-    if (pathname === "/web/") {
-      $target.innerHTML = `<div class="ProductListPage"></div>`;
-      const ProductListPage = $target.querySelector(".ProductListPage");
-      return new Main(ProductListPage);
-    } else if (pathname.includes("/web/products")) {
-      const id = pathname.split("/")[3];
-      $target.innerHTML = `<div class="ProductDetailPage"></div>`;
-      const ProductDetailPage = $target.querySelector(".ProductDetailPage");
-      return new Detail(ProductDetailPage, id);
-    } else if (pathname === "/web/cart") {
-      $target.innerHTML = `<div class="CartPage"></div>`;
-      const CartPage = $target.querySelector(".CartPage");
-      return new Cart(CartPage);
-    } else {
-      $target.innerHTML = `<div class="ProductListPage"></div>`;
-      const ProductListPage = $target.querySelector(".ProductListPage");
-      return new Main(ProductListPage);
-    }
+
+    const { html, className, page } = MAP_CONTAINER[pathname];
+    $target.innerHTML = html;
+    const $nextTarget = $target.querySelector(className);
+    page($nextTarget);
   };
 
   Route(route);
